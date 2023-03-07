1516d3e3135df04c8a6805e778c1330575988294
omi <momo.class5@gmail.com>
2023-03-07 15:30:01 +0900

    자바프로젝트 생성 HellowWorld

---
+++ JAVAEX/src/jungang/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package jungang.com;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("만나서 반가워~!");
+	}
+
+}
